447c8f6a217d50ebb0c6bc4275503e3019ebdea7
Tangogow <64528202+Tangogow@users.noreply.github.com>
2020-10-29 13:30:08 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,6 @@
-mkdir -p /tmp/test
-rm -rf /tmp/test/*
-echo '
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hello, world");
   }
 }
-' > /tmp/test/Main.java
-javac /tmp/test/Main.java
 
